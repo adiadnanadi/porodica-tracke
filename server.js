531821b2692ac5980e1a399584f22ff44762601f
adiadnanadi <adnansahanic40@gmail.com>
2026-03-15 02:16:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const path    = require("path");
 const app     = express();
 
-// CSP header koji dozvoljava Firebase
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googleapis.com https://*.gstatic.com https://unpkg.com; " +
-"connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://firestore.googleapis.com https://ipapi.co https://*.gstatic.com; " +
+    "connect-src 'self' https://*.googleapis.com https://*.firebaseio.com https://firestore.googleapis.com https://ipapi.co https://*.gstatic.com; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; " +
     "font-src 'self' https://fonts.gstatic.com; " +
     "img-src 'self' data: https://*.openstreetmap.org;"
@@ -16,10 +15,8 @@ app.use((req, res, next) => {
   next();
 });
 
-// Javna stranica
 app.use("/", express.static(path.join(__dirname, "public")));
 
-// Admin panel
 const ADMIN_USER = process.env.ADMIN_USER || "admin";
 const ADMIN_PASS = process.env.ADMIN_PASS || "tajna123";
 
